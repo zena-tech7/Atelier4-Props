b5e9a2afff74168b96331a1695c91ff2b08682d5
Hshella <163134999+HasnaChalla@users.noreply.github.com>
2025-12-24 17:03:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './styles.css';
 function CourseGoal(props) {
   return (
     <li>
-      <h2>{title = props.title}</h2>
-      <p>{size = props.description}</p>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
     </li>
   );
 }
@@ -17,10 +17,8 @@ function App() {
       <h1>"TIME TO PRACTICE"</h1>
       <h3>One course, many goals! 🎯</h3>
       <ul>
-        <CourseGoal
-          title={"Learn React"}
-          description={"In-depth"}
-        />
+        <CourseGoal title="Learn React"/>
+        <CourseGoal description="In-depth"/>
       </ul>
     </div>
   );
